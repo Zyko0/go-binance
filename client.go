fe7e57f8c79358f8ffbba6a79017107f64499f15
Zyko0 <bertrand.jung@epitech.eu>
2019-09-10 21:58:35 +0200

    Fixed binance.je endpoint

--- client.go
+++ client.go
@@ -76,7 +76,7 @@ func NewClient(apiKey, secretKey string) *Client {
 	return &Client{
 		APIKey:     apiKey,
 		SecretKey:  secretKey,
-		BaseURL:    "https://www.api.binance.je",
+		BaseURL:    "https://api.binance.je",
 		UserAgent:  "Binance/golang",
 		HTTPClient: http.DefaultClient,
 		Logger:     log.New(os.Stderr, "Binance-golang ", log.LstdFlags),
